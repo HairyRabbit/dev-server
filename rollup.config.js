749522cd381db3711a099b4400388b0e6c66f693
yuffiy <yfhj1990@hotmail.com>
2018-02-05 15:16:17 +0800

    remove lodash to devDeps

--- rollup.config.js
+++ rollup.config.js
@@ -28,8 +28,7 @@ export default [{
     }),
     babel({ exclude: 'node_modules/**' }),
     json({ exclude: 'node_modules/**' }),
-    commonjs(),
-    'production' === process.env.NODE_ENV ? uglify({}) : []
+    commonjs()
   ),
   external: [].concat(
     'repl',
